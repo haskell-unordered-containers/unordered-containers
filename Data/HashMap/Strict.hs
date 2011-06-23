c5b6be0df1517189f9ca316bee2db21cf6f892c8
Johan Tibell <johan.tibell@gmail.com>
2011-06-23 11:06:03 +0200

    Forgot to add new file

---
+++ Data/HashMap/Strict.hs
@@ -0,0 +1,12 @@
+module Data.HashMap.Strict
+    ( HashMap
+    , empty
+    , singleton
+    , HM.null
+    , insert
+    , HM.lookup
+    , fromList
+    , toList
+    ) where
+
+import Data.HashMap as HM
