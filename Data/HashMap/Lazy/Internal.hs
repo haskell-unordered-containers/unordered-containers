c0fea5dd108ceabc2535e9f785957bc0417f01d4
Bas van Dijk <v.dijk.bas@gmail.com>
2012-01-27 11:05:29 +0100

    Added Data.HashMap.Internal and Data.HashSet.Internal These modules export the constructors of the respected data types. Because Data.HashMap.Internal exports both the constructors of HashMap and List which both had a Nil constructor, I had to rename the HashMap.Nil to Empty. Both modules are marked as Unsafe and are hidden in the Haddock documentation to make it harder for users to accidentally use them.

--- Data/HashMap/Lazy/Internal.hs
+++ Data/HashMap/Lazy/Internal.hs
@@ -34,15 +34,15 @@ collisions t = go t 0
       | fl_sz <= 1 = sz
       | otherwise  = sz + fl_sz
       where fl_sz = FL.size l
-    go Nil           !sz = sz
+    go Empty         !sz = sz
 
 -- | /O(n)/ Return histogram of hash collisions in this map.
 -- Keys are number of entries in bucket, values are number of buckets
 -- of that size.
 collisionHistogram :: HashMap k v -> HashMap Int Int
-collisionHistogram t = go t Nil
+collisionHistogram t = go t Empty
   where
     go (Bin _ l r) h = go r (go l h)
     go (Tip _ l)     h = (insert sz $! maybe 1 (1+) (lookup sz h)) h
       where sz = FL.size l
-    go Nil           h = h
+    go Empty         h = h
