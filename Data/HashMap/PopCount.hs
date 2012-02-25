7c19a6d9e5726eb983d14114f5a9b88e498b6351
Johan Tibell <johan.tibell@gmail.com>
2012-02-25 08:20:40 -0800

    Conditionally import CUInt constructor on GHC >= 7.2

--- Data/HashMap/PopCount.hs
+++ Data/HashMap/PopCount.hs
@@ -1,11 +1,16 @@
-{-# LANGUAGE ForeignFunctionInterface #-}
+{-# LANGUAGE CPP, ForeignFunctionInterface #-}
 
 module Data.HashMap.PopCount
     ( popCount
     ) where
 
 import Data.Word (Word)
+
+#if __GLASGOW_HASKELL__ >= 702
+import Foreign.C (CUInt(..))
+#else
 import Foreign.C (CUInt)
+#endif
 
 foreign import ccall unsafe "popc.h popcount" c_popcount :: CUInt -> CUInt
 
