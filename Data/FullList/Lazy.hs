461dcebe2e268ab2b5f2e45a732924a3de3f4137
Bas van Dijk <v.dijk.bas@gmail.com>
2012-02-27 17:34:24 +0100

    Added function insertLookupWithKey This function is similar to the equivalently named function in Data.Map and Data.IntMap.
    O(min(n,W)). The expression (insertLookupWithKey' f k x map)
    is a pair where the first element is equal to (lookup k map)
    and the second element equal to (insertWith' (f k) k x map).
    
    insertLookupWithKey :: (Eq k, Hashable k)
                        => (k -> v -> v -> v) -> k -> v -> HashMap k v
                        -> (Maybe v, HashMap k v)

--- Data/FullList/Lazy.hs
+++ Data/FullList/Lazy.hs
@@ -27,6 +27,7 @@ module Data.FullList.Lazy
     , insert
     , delete
     , insertWith
+    , insertLookupWith
     , adjust
 
       -- * Combine
@@ -206,6 +207,25 @@ insertWithL = go
         | otherwise = Cons k' v' (go f k v xs)
 #if __GLASGOW_HASKELL__ >= 700
 {-# INLINABLE insertWithL #-}
+#endif
+
+insertLookupWith :: Eq k => (v -> v -> v) -> k -> v -> FullList k v -> (Maybe v, FullList k v)
+insertLookupWith f !k v (FL k' v' xs)
+    | k == k'   = (Just v', FL k (f v v') xs)
+    | otherwise = let (found, l) = insertLookupWithL f k v xs in (found, FL k' v' l)
+#if __GLASGOW_HASKELL__ >= 700
+{-# INLINABLE insertLookupWith #-}
+#endif
+
+insertLookupWithL :: Eq k => (v -> v -> v) -> k -> v -> List k v -> (Maybe v, List k v)
+insertLookupWithL = go
+  where
+    go _ !k v Nil = (Nothing, Cons k v Nil)
+    go f k v (Cons k' v' xs)
+        | k == k'   = (Just v', Cons k (f v v') xs)
+        | otherwise = let (found, l) = go f k v xs in (found, Cons k' v' l)
+#if __GLASGOW_HASKELL__ >= 700
+{-# INLINABLE insertLookupWithL #-}
 #endif
 
 adjust :: Eq k => (v -> v) -> k -> FullList k v -> FullList k v
