37068ca8e8eaafcd557c3d97821af4160ba600f4
Johan Tibell <johan.tibell@gmail.com>
2012-02-29 17:52:30 -0800

    Use popCnt# primop, where available

--- Data/HashMap/PopCount.hs
+++ Data/HashMap/PopCount.hs
@@ -4,15 +4,20 @@ module Data.HashMap.PopCount
     ( popCount
     ) where
 
-import Data.Word (Word)
-
-#if __GLASGOW_HASKELL__ >= 702
-import Foreign.C (CUInt(..))
+#if __GLASGOW_HASKELL__ >= 704
+import Data.Bits (popCount)
 #else
+import Data.Word (Word)
+# if __GLASGOW_HASKELL__ >= 702
+import Foreign.C (CUInt(..))
+# else
 import Foreign.C (CUInt)
+# endif
 #endif
 
+#if __GLASGOW_HASKELL__ < 704
 foreign import ccall unsafe "popc.h popcount" c_popcount :: CUInt -> CUInt
 
 popCount :: Word -> Int
 popCount w = fromIntegral (c_popcount (fromIntegral w))
+#endif
