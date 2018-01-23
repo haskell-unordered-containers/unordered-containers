fe0bdf42402dc4eb9d5c7dc4912fb76f24701f14
Matt Renaud <matt@m-renaud.com>
2018-01-22 18:48:03 -0800

    Add operator HashMap.!?.
    This is equivalent to `lookup` with the arguments flipped. This operator was
    added to containers-0.5.9.1, this makes the HashMap interface consistent.
    
    This partially addresses
    https://github.com/tibbe/unordered-containers/issues/172.

--- Data/HashMap/Base.hs
+++ Data/HashMap/Base.hs
@@ -19,6 +19,7 @@ module Data.HashMap.Base
     , size
     , member
     , lookup
+    , (!?)
     , lookupDefault
     , (!)
     , insert
@@ -447,6 +448,17 @@ lookup k0 m0 = go h0 k0 0 m0
         | h == hx   = lookupInArray k v
         | otherwise = Nothing
 {-# INLINABLE lookup #-}
+
+-- | /O(log n)/ Return the value to which the specified key is mapped,
+-- or 'Nothing' if this map contains no mapping for the key.
+--
+-- This is a flipped version of 'lookup'.
+--
+-- @since 0.2.9
+(!?) :: (Eq k, Hashable k) => HashMap k v -> k -> Maybe v
+(!?) m k = lookup k m
+{-# INLINE (!?) #-}
+
 
 -- | /O(log n)/ Return the value to which the specified key is mapped,
 -- or the default value if this map contains no mapping for the key.
