37b3b805e9283ba7fc3baa5582ac8c76584f2a06
Emily Pillmore <emilypi@cohomolo.gy>
2020-05-31 12:35:44 -0400

    Update Data/HashMap/Base.hs
    Co-authored-by: Simon Jakobi <simon.jakobi@gmail.com>

--- Data/HashMap/Base.hs
+++ Data/HashMap/Base.hs
@@ -1126,9 +1126,9 @@ adjust# f k0 m0 = go h0 k0 0 m0
         | otherwise = t
 {-# INLINABLE adjust# #-}
 
--- | /O(log n)/  The expression (@'update' f k map@) updates the value @x@ at @k@
--- (if it is in the map). If (@f x@) is @'Nothing'@, the element is deleted.
--- If it is (@'Just' y@), the key @k@ is bound to the new value @y@.
+-- | /O(log n)/  The expression @('update' f k map)@ updates the value @x@ at @k@
+-- (if it is in the map). If @(f x)@ is 'Nothing', the element is deleted.
+-- If it is @('Just' y)@, the key @k@ is bound to the new value @y@.
 update :: (Eq k, Hashable k) => (a -> Maybe a) -> k -> HashMap k a -> HashMap k a
 update f = alter (>>= f)
 {-# INLINABLE update #-}
