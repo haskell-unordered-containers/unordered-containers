aa29842c3dc2bc56a5226baae61fefd1a547e77c
Tristan McLeay <cassowary@fastmail.fm>
2020-06-03 14:29:42 +0200

    Update Data/HashMap/Base.hs
    Co-authored-by: Emily Pillmore <emilypi@cohomolo.gy>

--- Data/HashMap/Base.hs
+++ Data/HashMap/Base.hs
@@ -357,8 +357,7 @@ instance Ord k => Ord1 (HashMap k) where
 -- /Note:/ The ordering of elements in 'HashMap' depends primarily on the keys'
 -- hashes. Therefore, it inherits any of the properties of the hashes. In
 -- particular, if the hash in use is inconsistent between library versions,
--- OSes or architectures, then the ordering will be inconsistent in the same
--- circumstance.
+-- OSes or architectures, then the ordering will reflect these inconsistencies. 
 instance (Ord k, Ord v) => Ord (HashMap k v) where
     compare = cmp compare compare
 
