ec242154bf4b5d5b7a776878e3decfb14642c5d4
David Feuer <David.Feuer@gmail.com>
2022-04-09 16:54:38 -0400

    Try the unboxed result thing
    This one inlines the unboxed form into everything else, hopefully.

--- Data/HashMap/Internal.hs
+++ Data/HashMap/Internal.hs
@@ -1769,20 +1769,24 @@ intersectionWith f = inline intersectionWithKey $ const f
 -- the provided function is used to combine the values from the two
 -- maps.
 intersectionWithKey ::  (Eq k, Hashable k) => (k -> v1 -> v2 -> v3) -> HashMap k v1 -> HashMap k v2 -> HashMap k v3
-intersectionWithKey f = go 0
+intersectionWithKey f = intersectionWithKey# (\k v1 v2 -> (# f k v1 v2 #))
+{-# INLINABLE intersectionWithKey #-}
+
+intersectionWithKey# ::  (Eq k, Hashable k) => (k -> v1 -> v2 -> (# v3 #)) -> HashMap k v1 -> HashMap k v2 -> HashMap k v3
+intersectionWithKey# f = go 0
   where
     -- empty vs. anything
     go !_ _ Empty = Empty
     go _ Empty _ = Empty
     -- leaf vs. anything
-    go s (Leaf h1 (L k1 v1)) t2 = lookupCont (\_ -> Empty) (\v _ -> Leaf h1 $ L k1 $ f k1 v1 v) h1 k1 s t2
-    go s t1 (Leaf h2 (L k2 v2)) = lookupCont (\_ -> Empty) (\v _ -> Leaf h2 $ L k2 $ f k2 v v2) h2 k2 s t1
+    go s (Leaf h1 (L k1 v1)) t2 = lookupCont (\_ -> Empty) (\v _ -> case f k1 v1 v of (# v' #) -> Leaf h1 $ L k1 v') h1 k1 s t2
+    go s t1 (Leaf h2 (L k2 v2)) = lookupCont (\_ -> Empty) (\v _ -> case f k2 v v2 of (# v' #) -> Leaf h2 $ L k2 v') h2 k2 s t1
     -- collision vs. collision
     go _ (Collision h1 ls1) (Collision h2 ls2)
       | h1 == h2 = if A.length ls == 0 then Empty else Collision h1 ls
       | otherwise = Empty
       where
-        ls = intersectionUnorderedArrayWithKey (\k v1 v2 -> (# f k v1 v2 #)) ls1 ls2
+        ls = intersectionUnorderedArrayWithKey f ls1 ls2
     -- branch vs. branch
     go s (BitmapIndexed b1 ary1) (BitmapIndexed b2 ary2) = intersectionArray s b1 b2 ary1 ary2
     go s (BitmapIndexed b1 ary1) (Full ary2) = intersectionArray s b1 fullNodeMask ary1 ary2
@@ -1818,7 +1822,7 @@ intersectionWithKey f = go 0
     normalize b ary
       | A.length ary == 0 = Empty
       | otherwise = bitmapIndexedOrFull b ary
-{-# INLINABLE intersectionWithKey #-}
+{-# INLINE intersectionWithKey# #-}
 
 intersectionArrayBy :: (v1 -> v2 -> HashMap k v) -> Bitmap -> Bitmap -> A.Array v1 -> A.Array v2 -> (Bitmap, A.Array (HashMap k v))
 intersectionArrayBy f = intersectionArrayByFilter f $ \case Empty -> False; _ -> True
