19caa3d9b17e2dd6cb544501f7968646362534a9
Simon Jakobi <simon.jakobi@gmail.com>
2025-10-18 07:40:02 +0200

    Setup benchmarks for fromList with Bytes and Int keys

---
+++ benchmarks/Key/Bytes.hs
@@ -0,0 +1,30 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+
+module Key.Bytes where
+
+import Control.Monad (replicateM)
+import Data.ByteString.Short
+import Data.Hashable
+import System.Random.Stateful
+import Control.DeepSeq
+
+newtype Bytes = Bytes {unBytes :: ShortByteString}
+  deriving (Eq, Hashable, Show, NFData)
+
+genBytes ::
+  (StatefulGen g m) =>
+  Int ->
+  g ->
+  m Bytes
+genBytes len gen = Bytes <$> uniformShortByteStringM len gen
+
+genNBytes ::
+  (StatefulGen g m) =>
+  Int ->
+  Int ->
+  g ->
+  m [Bytes]
+genNBytes n len = replicateM n . genBytes len
+
+instance Uniform Bytes where
+  uniformM = genBytes 32 
